d190d45270c53d00bc6aabe7add983311f138187
reemagoud0928 <reemagoud09@gmail.com>
2024-07-02 10:25:57 +0530

    Add files via upload

---
+++ ParseInt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ParseInt {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+       String str1="237";
+              String str2="562";
+
+        System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));
+        }
+}
