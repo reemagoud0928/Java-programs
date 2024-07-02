9c5c09a0e66df514d91928df57c0eb0ee6d50cbd
reemagoud0928 <reemagoud09@gmail.com>
2024-07-02 09:45:38 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class Substring {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=("java is simple language");
+        System.out.println(str.substring(6));
+                System.out.println(str.substring(3,15));
+}
+}
