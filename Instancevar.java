3f377f3ef4bf32066747770519092ecb65f81e09
reemagoud0928 <reemagoud09@gmail.com>
2024-07-04 12:08:55 +0530

    Add files via upload

---
+++ Instancevar.java
@@ -0,0 +1,19 @@
+class Sample
+{
+    int a;
+    long b;
+    double c;
+    String d;
+}
+        class Instancevar
+        {
+        public static void main(String[] args)
+        {
+       Sample s1=new Sample();
+       System.out.println(s1.a);
+       System.out.println(s1.b);
+       System.out.println(s1.c);
+       System.out.println(s1.d);
+        
+    }
+}
